040b325823047a82e4e66abdef55ee9b5f25988a
XiaofeiPang <1743896531@qq.com>
2020-02-14 11:17:03 +0800

    测试github

--- src/common/Test.java
+++ src/common/Test.java
@@ -4,6 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("yuan");
         System.out.println("pang");
+        System.out.println("fei");
     }
 
 }
